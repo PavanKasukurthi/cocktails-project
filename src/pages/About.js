a188574336721bf30262f02ac9588644d8421eda
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-02-26 22:31:27 +0530

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia itaque
+        sint nisi, recusandae beatae quia architecto, ullam unde, harum
+        voluptatem quas adipisci minima. Iste cumque impedit, labore officia
+        quos provident.
+      </p>
+    </section>
+  )
+}
+
+export default About
